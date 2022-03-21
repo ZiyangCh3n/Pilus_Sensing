19b8ff0f9d77197344d052f8c428ceb948cfec1f
ZiyangC <littleseashrimp@gmail.com>
2022-03-20 21:20:22 -0400

    test

--- image_process.py
+++ image_process.py
@@ -8,7 +8,7 @@ import subprocess
 
 DATA_DIR = r'D:\Researchdata\ZY1'
 BFTOOL_DIR = r'C:\Users\littl\Documents\Research\pilus\bftools'
-skip_preprocess = True
+skip_preprocess = False
 
 def Rename_Files():
     ref = {}
@@ -49,21 +49,21 @@ if __name__ == '__main__':
         Rename_Files()
         GetTiff()  
 
-    dir_list = {}
-    for parent, dir, file in os.walk(DATA_DIR):
-        if not len(dir):
-            bsname = os.path.basename(parent).split('_')[-1]
-            dir_list[bsname] = [os.path.join(parent, f) for f in file]
+    # dir_list = {}
+    # for parent, dir, file in os.walk(DATA_DIR):
+    #     if not len(dir):
+    #         bsname = os.path.basename(parent).split('_')[-1]
+    #         dir_list[bsname] = [os.path.join(parent, f) for f in file]
 
 
-    for tiff_dir, nd2_dir in zip(dir_list['tiff'], dir_list['nd2']):
-        nd2_img = ND2Reader(nd2_dir)
-        tiff_stack = io.imread(tiff_dir)
-        channels = nd2_img.metadata['channels']
-        timestep = np.round(nd2_img.timesteps / 1000 / 60, 0)
-        print(tiff_dir)
-        print(timestep)
-        n_timepoint = tiff_stack.shape[0]
+    # for tiff_dir, nd2_dir in zip(dir_list['tiff'], dir_list['nd2']):
+    #     nd2_img = ND2Reader(nd2_dir)
+    #     tiff_stack = io.imread(tiff_dir)
+    #     channels = nd2_img.metadata['channels']
+    #     timestep = np.round(nd2_img.timesteps / 1000 / 60, 0)
+    #     print(tiff_dir)
+    #     print(timestep)
+    #     n_timepoint = tiff_stack.shape[0]
 # for img, nd2 in zip(img_dir, nd2_dir):
 #     nd2_img = ND2Reader(nd2)
 #     img_stack = io.imread(img)
