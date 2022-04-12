08321271db5f17e39f891941526ddab0fcdb640c
Ziyang <721358757@qq.com>
2022-04-12 13:56:51 -0400

    calc_fluo_region

--- codes/calc_fluo_region.py
+++ codes/calc_fluo_region.py
@@ -22,11 +22,10 @@ if not int(ARG[3]): # set to 0 to run through all files
     ARG[3] = 10000
 STOP = int(ARG[2]) + int(ARG[3]) - 1
 
-def GetImageBgHist(img, channel, paras_rb = [50, 50]):
-    img_raw = img[..., CHANNEL[channel]]
+def GetImageBgHist(img, channel, tp, paras_rb = [50, 50]):
+    img_raw = img[tp, ..., CHANNEL[channel]]
     bg = rolling_ball(img_raw, radius = paras_rb[0])
     bg_normal = img_as_uint(rank.mean(img_as_uint(bg.astype(int)), selem = disk(paras_rb[1])))
-    bg_normal = img_as_uint(rank.mean())
     img_bg_reduced = img_raw - bg_normal
     img_bg_reduced[img_raw < bg_normal] = 0
     # img_masked = img_bg_reduced * mask
@@ -49,16 +48,19 @@ def GetMaskRegion(mask, centroid0, ids0, area_thr = 100):
         dist = cdist(centroid, centroid0, metric='euclidean')
         match = linear_sum_assignment(dist)
         for r, r0 in zip(match[0], match[1]):
-            mask_labeled[mask_labeled == ids[r]] = ids0[r0]
+            region = props[r]
+            mask_labeled[region.coords[:, 0], region.coords[:, 1]] = ids0[r0]
+        # for r, r0 in zip(match[0], match[1]):
+        #     mask_labeled[mask_labeled == ids[r]] = ids0[r0]
         ids = ids0[match[1]]
         centroid = centroid[match[0]]
     
     return mask_labeled, centroid, ids
 
 # Calculate fluo of each position-timepoint img
-def CalcFluoByRegion(img, mask, centroid0, ids0, filename):
-    yfp_bg_reduced, yfp_bg, yfp_hist = GetImageBgHist(img, 'YFP')
-    mc_bg_reduced, mc_bg, mc_hist = GetImageBgHist(img, 'mcherry')
+def CalcFluoByRegion(img, mask, centroid0, ids0, filename, tp):
+    yfp_bg_reduced, yfp_bg, yfp_hist = GetImageBgHist(img, 'YFP', tp)
+    mc_bg_reduced, mc_bg, mc_hist = GetImageBgHist(img, 'mcherry', tp)
     mask_labeled, centroid, ids = GetMaskRegion(mask, centroid0, ids0)
     props = regionprops(mask_labeled)
     stats = {}
@@ -66,7 +68,7 @@ def CalcFluoByRegion(img, mask, centroid0, ids0, filename):
     # visualization
     fig, ax = plt.subplots(nrows = 3, ncols = 3, figsize = (15, 15))
     c0, c1 = 'YFP', 'mcherry'
-    ax[0, 0].imshow(img[..., CHANNEL[c0]], cmap = 'gray')
+    ax[0, 0].imshow(img[tp, ..., CHANNEL[c0]], cmap = 'gray')
     ax[0, 0].set_title("%s image raw" % c0)
     ax[0, 0].axis('off')
     ax[0, 1].imshow(yfp_bg_reduced, cmap = 'gray')
@@ -76,7 +78,7 @@ def CalcFluoByRegion(img, mask, centroid0, ids0, filename):
     ax[0, 2].legend(loc = 'upper left')
     ax[0, 2].set_title("%s histogram" % c0)
     
-    ax[1, 0].imshow(img[..., CHANNEL[c1]], cmap = 'gray')
+    ax[1, 0].imshow(img[tp, ..., CHANNEL[c1]], cmap = 'gray')
     ax[1, 0].set_title("%s image raw" % c1)
     ax[1, 0].axis('off')
     ax[1, 1].imshow(mc_bg_reduced, cmap = 'gray')
@@ -86,11 +88,11 @@ def CalcFluoByRegion(img, mask, centroid0, ids0, filename):
     ax[1, 2].legend(loc = 'upper left')
     ax[1, 2].set_title("%s histogram" % c1)
     
-    ax[2, 1].imshow(yfp_bg_reduced * mask, cmap = 'gray')
-    ax[2, 1].set_title("%s masked" % c0)
+    ax[2, 1].imshow(yfp_bg, cmap = 'gray')
+    ax[2, 1].set_title("%s BG" % c0)
     ax[2, 1].axis('off')
-    ax[2, 2].imshow(mc_bg_reduced * mask, cmap = 'gray')
-    ax[2, 2].set_title("%s masked" % c1)    
+    ax[2, 2].imshow(mc_bg, cmap = 'gray')
+    ax[2, 2].set_title("%s BG" % c1)    
     ax[2, 2].axis('off')
     
     ax[2, 0].imshow(mask, cmap = 'gray')
@@ -115,18 +117,15 @@ def CalcFluoByRegion(img, mask, centroid0, ids0, filename):
     
     
     plt.tight_layout()
-    plt.savefig(filename, bbox_inches = 'tight')
+    plt.savefig(path.join(data_dir, 'fluo_ref', filename), bbox_inches = 'tight')
     plt.close()
-    
+    stats = pd.DataFrame.from_dict(stats, orient = 'index').reset_index().rename(columns = {'index': 'timepoint'})
     return centroid, ids, stats
-    df = pd.DataFrame.from_dict(stats, orient='index').reset_index().rename(columns={'index': 'Region'})
-    df.to_csv(path.join(data_dir, 'fluorescence.csv'), index = False, mode = 'a',
-              header = not path.exists(path.join(data_dir, 'fluorescence.csv')))
 
 def CalcFluoMain(img, mask_dir, position):
     centroid0 = []
     ids0 = []
-    dict_df = {}
+    # dict_df = {}
     
     for parent, folder, file in walk(mask_dir):
         for f in file:
@@ -136,8 +135,18 @@ def CalcFluoMain(img, mask_dir, position):
                 # if path.exists(path.join(data_dir, 'fluo_ref', f)):
                 #     continue
                 mask = io.imread(path.join(parent, f))
-                centroid0, ids0, stats = CalcFluoByRegion(img, mask, centroid0, ids0, f)
-                dict_df[tp_mask] = stats
+                centroid0, ids0, stats = CalcFluoByRegion(img, mask, centroid0, ids0, f, tp_mask)
+                try:
+                    df = pd.concat([df, stats])
+                except NameError:
+                    df = stats    
+
+        
+    # for key, val in zip(dict_df.keys(), dict_df.values()):
+    #     dict_df[key] = pd.DataFrame.from_dict(val, orient = 'index').reset_index().rename(columns = {'index': 'timepoint'})
+        df.to_csv(path.join(data_dir, 'fluorescence.csv'), index = False, mode = 'a',
+              header = not path.exists(path.join(data_dir, 'fluorescence.csv')))
+        # return df
                 
 if __name__ == '__main__':
     LOC = 0
@@ -162,7 +171,10 @@ if __name__ == '__main__':
             t1 = time.time()
             if flag:
                 with open(path.join(data_dir, 'log'), 'a') as log:
-                    log.write()
-                                    
+                    log.write('-'*10 + 'Fluorescence' + '-'*10 + '\n')
+                    log.write('Started at: ' + T_START + '\n')
+                    log.write('DATA_DIR: ' + data_dir + '\n')
+                    log.write('START: %d STOP: %d \n' % (START % 36, STOP % 36))
+                    log.write('Totaltime in min: ' + str(round((t1 - t0) / 60, 2)) + '\n')
                                 
                         
